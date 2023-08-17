8b364e8dc0aa8d5164804b4d644b59da70ddc4ee
busraornek <ornekbuse9@gmail.com>
2023-08-17 13:05:41 +0300

    ekleme

---
+++ app/src/main/java/com/busraornek/odevbir/hafta5/soru1/Soru1.kt
@@ -0,0 +1,9 @@
+package com.busraornek.odevbir.hafta5.soru1
+
+/*
+1.ImageView ve TextView içeren bir nesne sınıfı tanımlayın. RecyclerView içerisinde 3 lü ve yatayda
+scroll edilebilecek şekilde kodlayınız.
+
+
+https://github.com/busraornek/RecyclerView_Kitap_Ornek
+ */
---
+++ app/src/main/java/com/busraornek/odevbir/hafta5/soru2/MediumYazisi.kt
@@ -0,0 +1,7 @@
+package com.busraornek.odevbir.hafta5.soru2
+
+/*
+Medium konusu Listeler, RecyclerView ve Adapter Tasarımı
+
+https://medium.com/@busraornek48/listeler-recyclerview-ve-adapter-tasar%C4%B1m%C4%B1-fa1c3c26e222
+ */
