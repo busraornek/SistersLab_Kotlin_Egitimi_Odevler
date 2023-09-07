1c7b353951012c915f2e4685565c28b5ead31c6e
busraornek <ornekbuse9@gmail.com>
2023-09-08 00:55:18 +0300

    ekleme

---
+++ app/src/main/java/com/busraornek/odevbir/hafta7/soru1/soru1.kt
@@ -0,0 +1,7 @@
+package com.busraornek.odevbir.hafta7.soru1
+
+/*
+Firebase Realtime Database kullanarak Notlar Uygulaması yapılacak RecyclerViewde notlar listelenecek notlar siline bilmeli güncellenebilmeli ve ekleme yapıla bilmeli ( tasarımı ve renkler size özgün olmalı
+
+https://github.com/busraornek/NotlarUygulamasi
+ */
---
+++ app/src/main/java/com/busraornek/odevbir/hafta7/soru2/soru2.kt
@@ -0,0 +1,11 @@
+package com.busraornek.odevbir.hafta7.soru2
+
+/*
+
+
+Firebase Realtime ve Firestore farkı nedir:
+https://medium.com/@busraornek48/firebase-realtime-database-ve-firestore-aras%C4%B1ndaki-farkl%C4%B1l%C4%B1klar-41d98fa75edb
+
+Firebase Realtime Database: Veri Ekleme, Silme ve Güncelleme İşlemleri
+https://medium.com/@busraornek48/firebase-realtime-database-veri-ekleme-silme-ve-g%C3%BCncelleme-i%CC%87%C5%9Flemleri-68c500256b45
+ */
