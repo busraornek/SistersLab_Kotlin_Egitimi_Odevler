f0d4b090189556cab98be65b7af277fd07abd8bf
busraornek <ornekbuse9@gmail.com>
2023-09-15 20:59:13 +0300

    ekleme

---
+++ app/src/main/java/com/busraornek/odevbir/hafta8/soru1/soru1.kt
@@ -0,0 +1,7 @@
+package com.busraornek.odevbir.hafta8.soru1
+
+/*
+Model-View-ViewModel (MVVM) teknolojisi nedir nedir ne için kullanılır diğer teknolojilerden farkı
+
+https://medium.com/@busraornek48/model-view-viewmodel-mvvm-teknolojisi-nedir-afc5d42777d7
+ */
---
+++ app/src/main/java/com/busraornek/odevbir/hafta8/soru2/soru2.kt
@@ -0,0 +1,7 @@
+package com.busraornek.odevbir.hafta8.soru2
+
+/*
+CocukEglence uygulaması tamamlanacak veriler firebase den
+ çekilecek texler Realtime database kullanılarak, görseller storage kullanılarak çekilecek
+https://github.com/busraornek/CocukEglence
+ */
